e21b66230ea35e18ebc3b030e9cda7131be88ee9
alexjirik <alex@roundpegconsulting.com>
2026-06-10 13:44:12 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Surf & Coffee Blog", page_icon="☕")
+
+st.title("Welcome to my Streamlit App! 🌊")
+st.write("Just catching some waves and drinking good coffee.")
